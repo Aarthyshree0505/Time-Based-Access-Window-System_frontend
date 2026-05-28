d902544bdb9abe646ba30f5d7adc4b24c9c7fb30
Aarthy Shree05 <e0423038@sriher.edu.in>
2026-05-28 12:00:45 +0530

    Updated backend API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://time-based-access-window-system-backend.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
